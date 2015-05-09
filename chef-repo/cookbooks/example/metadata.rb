32bfc6e9dabc39a4bb2d6cb8b5911d383aaf2c0c
sc0ttruss <scott.russell@inetmedia.co.uk>
2015-05-09 18:03:59 +0300

    Update metadata.rb
    have to have a maintainer field or hosted chef does not seem to like it when uploading the cookbook, have also bumped the version just to be sure

--- chef-repo/cookbooks/example/metadata.rb
+++ chef-repo/cookbooks/example/metadata.rb
@@ -1,3 +1,6 @@
 name 'example'
+maintainer       "Opscode, Inc."
+maintainer_email "cookbooks@opscode.com"
+license          "Apache 2.0"
 description 'An example cookbook'
-version '1.0.0'
+version '1.0.1'
